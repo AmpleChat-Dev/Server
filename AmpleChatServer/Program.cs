abefb65dd5df0f4ba0dbd4095dcfa065cfe10188
Boris <Borismulder@outlook.com>
2021-10-11 01:38:15 +0200

    Testing ws connection
    Made a bas application to see how websockets communicate

--- AmpleChatServer/Program.cs
+++ AmpleChatServer/Program.cs
@@ -1,14 +1,29 @@
 ﻿using WebSocketSharp.Server;
+using System;
 
 namespace AmpleChatServer
 {
     class Program
     {
+        static readonly int PORT_NUM = 49152;
+        static readonly string URL = $"ws://localhost:{PORT_NUM}";
+        static readonly string EXIT_COMMAND = "exit";
+
         static void Main(string[] args)
         {
-            var ws = new WebSocketServer(49152);
+            var ws = new WebSocketServer(URL);
 
+            ws.AddWebSocketService("/do_you_know_the_way", () => new WayPoint { });
+            
             ws.Start();
+
+            Console.WriteLine($"Server running on : {URL}");
+
+            while (Console.ReadLine() != EXIT_COMMAND);
+
+            ws.Stop();
+
+            Console.WriteLine("Server stopped, system exit");
         }
     }
 }
