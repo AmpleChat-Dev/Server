d00d5d77e30c7030824b553c4d9d37091fe6de8a
Boris <Borismulder@outlook.com>
2021-11-20 18:27:25 +0100

    Update Program.cs

--- AmpleChatServer/Program.cs
+++ AmpleChatServer/Program.cs
@@ -10,7 +10,7 @@ namespace AmpleChatServer {
         static readonly int PORT_NUM = 49152;
         static readonly string URL = $"ws://localhost:{PORT_NUM}";
         static readonly string EXIT_COMMAND = "exit";
-        static readonly string API_URL = "http//localhost:44396/api/";
+        static readonly string API_URL = "http://localhost:44396/api/";
 
         static void Main(string[] args) {
             // https://www.thecodebuzz.com/using-httpclientfactory-in-net-core-console-application/
@@ -30,21 +30,33 @@ namespace AmpleChatServer {
                 BaseAddress = new Uri(API_URL)
             };
 
-
             var ws = new WebSocketServer(URL);
 
-            ws.AddWebSocketService("/account", () => new AccountService(client));
+            ws.AddWebSocketService("/account", () => new AccountService {
+                ApiController = "account",
+                HttpClient = client
+            });
             ws.AddWebSocketService("/chat", () => new ChatService());
 
             ws.Start();
 
             Console.WriteLine($"Server running on : {URL}");
 
-            while (Console.ReadLine() != EXIT_COMMAND) ;
+            var cmd = string.Empty;
+            var read = true;
+
+            while (read) 
+            {
+                if (cmd == EXIT_COMMAND) // I0 ????
+                    read = false;
+
+
+                cmd = Console.ReadLine();
+            }
 
             ws.Stop();
 
-            Console.WriteLine("Server stopped, system exit");
+            Console.WriteLine("System exit");
         }
     }
 }
